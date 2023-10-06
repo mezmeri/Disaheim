5c5b64a71544ea35c67cee39a119487ee7682f6a
molbe <molbe@MPMOLAPTOP>
2023-10-06 09:08:44 +0200

    Add project files.

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+
+    internal class Amulet
+    {
+        public string ItemID { get; set; }
+        public string Design {  get; set; }
+        public Level Quality {  get; set; }
+
+        public Amulet(string ItemID)
+        {
+
+        }
+        public Amulet(string ItemID, Level Quality)
+        {
+
+        }
+        public Amulet (string ItemID, Level quality, string design)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ Disaheim/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        public string ItemID {  get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public Book (string ItemID)
+        {
+
+        }
+        public Book (string ItemId, string Title)
+        {
+
+        }
+        public Book (string ItemID, string Title, double Price)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
