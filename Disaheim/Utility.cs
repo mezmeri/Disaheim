a7952775adb0382bb63138f8264b01b9790ef40a
mezmeri <madsschmidt33@gmail.com>
2023-10-25 09:20:48 +0200

    added utility

---
+++ Disaheim/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    internal class Utility
+    {
+        public double GetValueOfBook(Book book)
+        {
+            return 0;
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            return 0;
+        }
+
+    }
+}
